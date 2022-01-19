f210c1aea307d1318457ed2514d0ff8aa254f93f
dulitharuvin <dulitha.ruvin@gmail.com>
2022-01-19 14:50:51 +0100

    table sorting logic to create tables in database done

--- src/import-data-base/import-data-base.service.ts
+++ src/import-data-base/import-data-base.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { readFileSync } from 'fs';
 import { Table } from './dto/Table';
+import { Column } from './dto/Column';
 
 @Injectable()
 export class ImportDataBaseService {
@@ -11,14 +12,51 @@ export class ImportDataBaseService {
       const tables: Table[] = jsonData.map((table: any) => {
         return new Table(table);
       });
-      this.sortJsonTableArray(tables);
-      return tables;
+      return this.sortJsonTableArray(tables);
     } catch (error) {
       console.log(`ERROR: ${error}`);
     }
   }
 
-  sortJsonTableArray(tables: Table[]) {
-    console.log(tables);
+  private sortJsonTableArray(tables: Table[]): Table[] {
+    const orderedTables: Table[] = [];
+    const tableQueToAddToOrdered: Table[] = [];
+    tables.forEach((tbl) => {
+      const foreignKeyTables = this.getForeignKeyReferencedTables(tbl.columns);
+      if (foreignKeyTables.length === 0) {
+        orderedTables.push(tbl);
+      } else if (
+        this.canInsertToOrderedTableList(foreignKeyTables, orderedTables)
+      ) {
+        orderedTables.push(tbl);
+      } else {
+        tableQueToAddToOrdered.push(tbl);
+      }
+    });
+    return [...orderedTables, ...tableQueToAddToOrdered];
+  }
+
+  private getForeignKeyReferencedTables(columns: Column[]): string[] {
+    const foreignKeyTables = columns.reduce(function (
+      cols: string[],
+      col: Column,
+    ) {
+      if (col.foreign_key) {
+        const tableName = col.foreign_key.split('.')[0];
+        cols.push(tableName);
+      }
+      return cols;
+    },
+    []);
+    return foreignKeyTables;
+  }
+
+  private canInsertToOrderedTableList(
+    tableNamesToCheck: string[],
+    orderedTables: Table[],
+  ): boolean {
+    return tableNamesToCheck.every((tblName) =>
+      orderedTables.some((tbl) => tbl.name === tblName),
+    );
   }
 }
